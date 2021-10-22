616429ba478615bf2be1de6a27bb15293d3ba816
Медведева Кристина Игоревна <medvedeva.k@tps.local>
2021-10-22 17:07:53 +0300

    'second'

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ecommerce/static/')
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
